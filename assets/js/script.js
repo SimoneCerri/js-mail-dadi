c6d63ca9e71e6357251850733334ce1c727d0081
SimoneCerri <iosonosimone@hotmail.it>
2024-02-22 15:37:42 +0100

    adjust comment on js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,28 @@ let mail = prompt("HEY ! Who are you ? Insert your email here..")
 
 console.log(mail);
 
+//check email in the list.
 for (let index = 0; index < existingMails.length; index++)
 {
     const check = existingMails[index];
+    //IF is in the list alert welcome.
     if (check === mail)
     {
         alert("WELCOME BACK BROH")
     }
-    else 
+    //ELSE you are not in the list.
+    else if (check !== mail)
     {
-        alert("You shall not pass..! cit.")
+        alert("You shall not pass..! cit.") 
     }
     
 }
 
+
+
+//DICE
+
+//generate a random number 1 - 6 for the player.
+//generate a random number 1 - 6 for the npc.
+//chose the winner(higher number).
+//alert with the winner.
