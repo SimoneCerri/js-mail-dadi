01cbafb8c79b6a4ce0e6e5cdf7fce26fa53c56b9
SimoneCerri <iosonosimone@hotmail.it>
2024-02-22 16:42:19 +0100

    style the page, need to work on js for the bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 //ask the client for an email.
 let existingMails = ["pippo@gmail.com","pluto@gmail.com","paperino@gmail.com"]
-let mail = prompt("HEY ! Who are you ? Insert your email here..")
+//let mail = prompt("HEY ! Who are you ? Insert your email here..")
 let checkMail = false;
 
 console.log(mail);
@@ -26,11 +26,11 @@ for (let index = 0; index < existingMails.length; index++)
 
 if (checkMail === true)
 {
-    alert("WELCOME BACK BROH")
+    //alert("WELCOME BACK BROH")
 }
 else 
 {
-    alert("You shall not pass..! cit.")
+    //alert("You shall not pass..! cit.")
 }
 
 
@@ -41,32 +41,34 @@ else
 let player = Math.random() * (6 - 1) + 1;
 //console.log(typeof player);
 player = Number(player.toFixed(0));
-console.log("Player roll: " + player);
+//console.log("Player roll: " + player);
 
 //generate a random number 1 - 6 for the npc.
 let npc = Math.random() * (6 - 1) + 1;
 npc = Number(npc.toFixed(0));
-console.log("Table roll: " + npc);
+//console.log("Table roll: " + npc);
 
 //chose the winner(higher number).
-let numbers = [];
-numbers.push(player);
-numbers.push(npc);
+let numbers = []; //useless but i had another plane before
+numbers.push(player); //useless but i had another plane before
+numbers.push(npc); //useless but i had another plane before
 //console.log(numbers);
 
 if ( npc === player)
 {
-    console.log("Same result, usually table win, but reroll..");
-    alert("Table roll: " + npc + " Player roll: " + player + " Same result, usually table win, but reroll..");
+    //console.log("Same result, usually table win, but reroll..");
+    //alert("Table roll: " + npc + " Player roll: " + player + " Same result, usually table win, but reroll..");//alert with the winner.
 }
 else if (npc > player)
 {
-    console.log("Table win..UNLUCKY!");
-    alert("Table roll: " + npc + " Player roll: " + player + " Table win..UNLUCKY!");
+    //console.log("Table win..UNLUCKY!");
+    //alert("Table roll: " + npc + " Player roll: " + player + " Table win..UNLUCKY!");//alert with the winner.
 }
 else if (player > npc)
 {
-    console.log("Player win..YOU GOT MONEY!");
-    alert("Table roll: " + npc + " Player roll: " + player + " Player win..YOU GOT MONEY!");
+    //console.log("Player win..YOU GOT MONEY!");
+    //alert("Table roll: " + npc + " Player roll: " + player + " Player win..YOU GOT MONEY!");//alert with the winner.
 }
-//alert with the winner.
+
+
+//BONUS
